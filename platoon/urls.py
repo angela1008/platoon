50a19d12eeb16edb3517cce4a65bec7434712c0f
albertan <albertanstar@gmail.com>
2016-12-04 17:56:32 +0800

    modify exchange_card.css index.css(for drawer) 	drawer html

--- platoon/urls.py
+++ platoon/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'', include('apps.landing.urls')),
+    url(r'', include('apps.exchange_card.urls')),
     url(r'^admin/', admin.site.urls),
 ]
